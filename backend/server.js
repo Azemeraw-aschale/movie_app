9d8a2afc952c8321f47b1bc17f937e2f38e5b3fc
Fasika <fasika@git.com>
2024-05-21 14:47:17 +0300

    azz

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-// const { pool} = require('./db');
-// const { pool } = require('./db'); 
+const { PrismaClient } = require('@prisma/client');
+
 const app = express();
 app.use(cors());
-// createTables();
+
+// Create an instance of the Prisma Client
+const prisma = new PrismaClient();
+
 // Other middleware and configurations
+
 // Import the addChannel router
 const addChannelRouter = require('./add_movie_cat_types/post_apis');
-const fetchMovieRouter= require('./fetch_movie_channel/fetch_apis');
-const updateMovieRouter=require('./update_data/update_');
-const userAuthRouter=require('./user_auth/user_auth');
+const fetchMovieRouter = require('./fetch_movie_channel/fetch_apis');
+const updateMovieRouter = require('./update_data/update_');
+const userAuthRouter = require('./user_auth/user_auth');
 
 app.use(express.json());
 
@@ -19,6 +23,15 @@ app.use(addChannelRouter);
 app.use(fetchMovieRouter);
 app.use(updateMovieRouter);
 app.use(userAuthRouter);
+
+// Check the database connection
+prisma.$connect()
+  .then(() => {
+    console.log('Database connected successfully!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
 
 // Start the server
 app.listen(8080, () => {
