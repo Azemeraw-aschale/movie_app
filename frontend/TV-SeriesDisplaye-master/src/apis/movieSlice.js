628678c5e5935f908a8e1470f67b93add1e1a6df
Azemeraw <109177630+Azemeraw-aschale@users.noreply.github.com>
2024-05-27 05:36:14 -0700

    Update movieSlice.js

--- frontend/TV-SeriesDisplaye-master/src/apis/movieSlice.js
+++ frontend/TV-SeriesDisplaye-master/src/apis/movieSlice.js
@@ -1,7 +1,7 @@
 // import { Try } from "@mui/icons-material";
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const APIURL = "http://localhost:8080/api";
+const APIURL = "https://movie-app-lumh.onrender.com/api";
 
 export const addMovie = createAsyncThunk(
   "add/program",
