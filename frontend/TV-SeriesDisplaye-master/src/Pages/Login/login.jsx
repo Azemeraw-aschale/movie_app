54e79af1d13e67f581069d6beb3bab33be0ca25a
Azemeraw <109177630+Azemeraw-aschale@users.noreply.github.com>
2024-05-27 00:12:25 -0700

    Update login.jsx

--- frontend/TV-SeriesDisplaye-master/src/Pages/Login/login.jsx
+++ frontend/TV-SeriesDisplaye-master/src/Pages/Login/login.jsx
@@ -1,15 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
+// import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Container } from "@mui/material";
+// import { Container } from "@mui/material";
 import axios from "axios";
 
 import Image from './logo.PNG';
