418ba3b14af1c9c843b874b8e976fa70728090a4
Azemeraw <109177630+Azemeraw-aschale@users.noreply.github.com>
2024-05-26 23:50:52 -0700

    Update App.js

--- frontend/TV-SeriesDisplaye-master/src/App.js
+++ frontend/TV-SeriesDisplaye-master/src/App.js
@@ -6,7 +6,7 @@ import Channel from './Pages/Channal/chanal';
 import Program from './Pages/Program/program';
 import Home from './HomePage';
 import Login from './Pages/Login/login'
-import CardContainer from './Pages/music_page/CardContainer';
+
 
 const App = () => {
   return (
